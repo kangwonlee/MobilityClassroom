1c5b1696c8c07e5e0b424645682d5d4508d4844f
Kangwon Lee (Education) <kangwonlee@users.noreply.github.com>
2022-10-10 21:49:19 +0900

    started test_func.py

---
+++ tests/test_func.py
@@ -0,0 +1,80 @@
+"""
+Test cases for .../utils/func.py file
+"""
+import configparser
+import math
+import os
+import sys
+import tempfile
+
+import pytest
+
+utils_path = os.path.abspath(
+    os.path.join(
+        os.pardir,
+        "src", "New Mobility", "utils"
+    )
+)
+
+assert os.path.exists(utils_path)
+
+sys.path.insert(0, utils_path,)
+
+import func
+
+
+@pytest.fixture
+def ky() -> float:
+    return 1.0
+
+
+@pytest.fixture
+def ka() -> float:
+    return 2.0
+
+
+@pytest.fixture
+def kcv() -> float:
+    return 3.0
+
+
+@pytest.fixture
+def kcl() -> float:
+    return 4.0
+
+
+@pytest.fixture
+def Vmax() -> float:
+    return 5.0
+
+
+@pytest.fixture
+def Aymax() -> float:
+    return 6.0
+
+
+@pytest.fixture
+def cfg_file_normal(ky, ka, kcv, kcl, Vmax, Aymax):
+    with tempfile.NamedTemporaryFile(delete=False) as f:
+        name = f.name
+        f.write(f"{ky}\n")
+        f.write(f"{ka}\n")
+        f.write(f"{kcv}\n")
+        f.write(f"{kcl}\n")
+        f.write(f"{Vmax}\n")
+        f.write(f"{Aymax}\n")
+    assert os.path.exists(name)
+    yield name
+    # remove config file after test
+    os.remove(name)
+
+
+def test_read_gain__normal(cfg_file_normal, ky, ka, kcv, kcl, Vmax, Aymax):
+    result = func.read_gain(cfg_file_normal)
+
+    assert math.isclose(result[0], ky)
+    assert math.isclose(result[1], ka)
+    assert math.isclose(result[2], kcv)
+    assert math.isclose(result[3], kcl)
+    assert math.isclose(result[4], Vmax)
+    assert math.isclose(result[5], Aymax)
