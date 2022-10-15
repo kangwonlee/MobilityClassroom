88659a7d3cd79e844d432f80f3b1623cc392bf78
Kangwon Lee (Education) <kangwonlee@users.noreply.github.com>
2022-10-16 01:20:49 +0900

    rgb fixtures return List[int] to simplify comparisons

--- tests/test_func_read_rgb_range.py
+++ tests/test_func_read_rgb_range.py
@@ -9,6 +9,9 @@ import sys
 import tempfile
 
 
+from typing import List
+
+
 import pytest
 
 
@@ -43,45 +46,45 @@ import func
 random.seed()
 
 @pytest.fixture
-def r1() -> str:
-    return ' '.join([str(random.randint(100, 199)) for k in range(3)])
+def r1() -> List[int]:
+    return [random.randint(100, 199) for k in range(3)]
 
 
 @pytest.fixture
-def r2() -> str:
-    return ' '.join([str(random.randint(200, 255)) for k in range(3)])
+def r2() -> List[int]:
+    return [random.randint(200, 255) for k in range(3)]
 
 
 @pytest.fixture
-def g1() -> str:
-    return ' '.join([str(random.randint(10, 49)) for k in range(3)])
+def g1() -> List[int]:
+    return [random.randint(10, 49) for k in range(3)]
 
 
 @pytest.fixture
-def g2() -> str:
-    return ' '.join([str(random.randint(50, 99)) for k in range(3)])
+def g2() -> List[int]:
+    return [random.randint(50, 99) for k in range(3)]
 
 
 @pytest.fixture
-def b1() -> str:
-    return ' '.join([str(random.randint(1, 4)) for k in range(3)])
+def b1() -> List[int]:
+    return [random.randint(1, 4) for k in range(3)]
 
 
 @pytest.fixture
-def b2() -> str:
-    return ' '.join([str(random.randint(5, 9)) for k in range(3)])
+def b2() -> List[int]:
+    return [random.randint(5, 9) for k in range(3)]
 
 
 @pytest.fixture
 def rgb_range_file_normal(r1, r2, g1, g2, b1, b2):
     txt = (
         "[range]"
-        f"r1 = {r1}\n"
-        f"r2= {r2}     \n"
-        f"g1 = {g1}     \n"
-        f"g2 = {g2}    \n"
-        f"b1 = {b1}\n"
-        f"b2 = {b2}\n"
+        f"r1 = {' '.join(map(str, r1))}\n"
+        f"r2= {' '.join(map(str, r2))}     \n"
+        f"g1 = {' '.join(map(str, g1))}     \n"
+        f"g2 = {' '.join(map(str, g2))}    \n"
+        f"b1 = {' '.join(map(str, b1))}\n"
+        f"b2 = {' '.join(map(str, b2))}\n"
     )
 
     with tempfile.NamedTemporaryFile(mode="wt", delete=False) as f:
@@ -100,12 +103,6 @@ def rgb_range_file_normal(r1, r2, g1, g2, b1, b2):
 def test_read_rgb_range__normal(rgb_range_file_normal, r1, r2, g1, g2, b1, b2):
     result = func.read_rgb_range(rgb_range_file_normal)
 
-    for k, v in enumerate(result):
-        assert isinstance(v, int), f"result[{k}] = {result[k]}"
+    expected = (r1, r2, g1, g2, b1, b2)
 
-    assert math.isclose(result[0], r1)
-    assert math.isclose(result[1], r2)
-    assert math.isclose(result[2], g1)
-    assert math.isclose(result[3], g2)
-    assert math.isclose(result[4], b1)
-    assert math.isclose(result[5], b2)
+    assert result == expected
