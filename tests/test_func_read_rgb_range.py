3bafa2958cf627de4383ddb1509b25ec1854e63b
Kangwon Lee (Education) <kangwonlee@users.noreply.github.com>
2022-10-16 00:20:03 +0900

    added a separated test file for read_rgb_range() function
    consider simplifying the tests/ folder

---
+++ tests/test_func_read_rgb_range.py
@@ -0,0 +1,109 @@
+"""
+Test cases for .../utils/func.py file
+"""
+import configparser
+import math
+import os
+import random
+import sys
+import tempfile
+
+import pytest
+
+src2_path = os.path.abspath(
+    os.path.join(
+        os.path.dirname(__file__),
+        os.pardir,
+        "src", "New Mobility",
+    )
+)
+
+assert os.path.exists(src2_path), (
+    f"unable to find {src2_path}\n"
+    f"current folder = {os.path.abspath(os.curdir)}"
+)
+
+utils_path = os.path.abspath(
+    os.path.join(src2_path, "utils"
+    )
+)
+
+assert os.path.exists(utils_path), (
+    f"unable to find {utils_path}\n"
+    f"current folder = {os.path.abspath(os.curdir)}"
+)
+
+sys.path.insert(0, src2_path,)
+sys.path.insert(0, utils_path,)
+
+import func
+
+random.seed()
+
+@pytest.fixture
+def r1() -> int:
+    return random.randint(0, 255)
+
+
+@pytest.fixture
+def r2() -> int:
+    return random.randint(0, 255)
+
+
+@pytest.fixture
+def g1() -> int:
+    return random.randint(0, 255)
+
+
+@pytest.fixture
+def g2() -> int:
+    return random.randint(0, 255)
+
+
+@pytest.fixture
+def b1() -> int:
+    return random.randint(0, 255)
+
+
+@pytest.fixture
+def b2() -> int:
+    return random.randint(0, 255)
+
+
+@pytest.fixture
+def rgb_range_file_normal(r1, r2, g1, g2, b1, b2):
+    txt = (
+        "[range]"
+        f"r1 = {r1}\n"
+        f"r2= {r2}     \n"
+        f"g1 = {g1}     \n"
+        f"g2 = {g2}    \n"
+        f"b1 = {b1}\n"
+        f"b2 = {b2}\n"
+    )
+
+    with tempfile.NamedTemporaryFile(mode="wt", delete=False) as f:
+        name = f.name
+        f.write(txt)
+
+    assert os.path.exists(name)
+
+    # run test
+    yield name
+
+    # remove config file after test
+    os.remove(name)
+
+
+def test_read_rgb_range__normal(rgb_range_file_normal, r1, r2, g1, g2, b1, b2):
+    result = func.read_cfg(rgb_range_file_normal)
+
+    for k, v in enumerate(result):
+        assert isinstance(v, float), f"result[{k}] = {result[k]}"
+
+    assert math.isclose(result[0], r1)
+    assert math.isclose(result[1], r2)
+    assert math.isclose(result[2], g1)
+    assert math.isclose(result[3], g2)
+    assert math.isclose(result[4], b1)
+    assert math.isclose(result[5], b2)
